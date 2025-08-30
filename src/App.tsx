aad7f8001cb3849af4378beea9f4c3baec10c476
chriskuschnar-coder <chriskuschnar@gmail.com>
2025-08-30 02:04:35 -0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { supabaseClient } from './lib/supabase-client'
 import { AuthProvider } from './components/auth/AuthProvider'
 import { InvestmentPlatform } from './components/InvestmentPlatform'
