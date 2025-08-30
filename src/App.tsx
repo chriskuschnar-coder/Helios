69d843a5f55e9fa13303b8af8785f25e7478a1e0
chriskuschnar-coder <chriskuschnar@gmail.com>
2025-08-30 02:28:31 -0400

    Show Beautiful Marketing Website First

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import React from 'react'
 import { AuthProvider } from './components/auth/AuthProvider'
+import { Hero } from './components/Hero'
+import { About } from './components/About'
+import { Services } from './components/Services'
+import { Performance } from './components/Performance'
+import { Contact } from './components/Contact'
+import { Header } from './components/Header'
+import { Footer } from './components/Footer'
 import { InvestmentPlatform } from './components/InvestmentPlatform'
 
 function App() {
+  const [showPortal, setShowPortal] = React.useState(false)
+
+  if (showPortal) {
+    return (
+      <AuthProvider>
+        <InvestmentPlatform />
+      </AuthProvider>
+    )
+  }
+
   return (
-    <AuthProvider>
-      <InvestmentPlatform />
-    </AuthProvider>
+    <main className="min-h-screen bg-white">
+      <Header onNavigateToLogin={() => setShowPortal(true)} />
+      <Hero />
+      <About />
+      <Services />
+      <Performance />
+      <Contact />
+      <Footer />
+    </main>
   )
 }
 
