a319fb0b9138b1f286f18160607ca6688741d119
chriskuschnar-coder <chriskuschnar@gmail.com>
2025-08-31 04:16:09 -0400

    Fix Stripe Key Syntax Error

--- src/components/StripeCardForm.tsx
+++ src/components/StripeCardForm.tsx
@@ -5,4 +5,4 @@ import { StripeCardFormInner } from './StripeCardFormInner'
 import { Loader2, Shield, CreditCard } from 'lucide-react'
 
 // Create Stripe promise - but we'll wait for it to fully resolve
-const STRIPE_PUBLISHABLE_KEY = 'pk_test_51S25DbFhEA0kH7xcn7HrWHyUNUgJfFaYiYmnAMLhBZeWE1fU9TLhiKKh6bTvJz3LF68E9qAokVRBJMHLWkiPWUR000jC
+const STRIPE_PUBLISHABLE_KEY = 'pk_test_51S25DbFhEA0kH7xcn7HrWHyUNUgJfFaYiYmnAMLhBZeWE1fU9TLhiKKh6bTvJz3LF68E9qAokVRBJMHLWkiPWUR000jCr1fLmH'
