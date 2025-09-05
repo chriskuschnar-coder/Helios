2ada16f2707f4b68fdae476f4e26eef7ad0b1af7
autoprintsai-code <autoprintsai@gmail.com>
2025-09-05 03:35:16 -0700

    Fix HeliosDashboard component function structure

--- src/components/HeliosDashboard.tsx
+++ src/components/HeliosDashboard.tsx
@@ -1,4 +1,9 @@
-return (
+import React from 'react';
+import { Zap, ExternalLink, Activity } from 'lucide-react';
+import TickerTape from './TickerTape';
+
+const HeliosDashboard: React.FC = () => {
+  return (
     <div className="min-h-screen bg-gray-900 safe-area-bottom">
       {/* TradingView Ticker Tape */}
       <div className="bg-gray-800 border-b border-gray-700">
@@ -63,3 +68,8 @@ return (
           </div>
         </div>
       </div>
+    </div>
+  );
+};
+
+export default HeliosDashboard;
