bf9301a9c1696fdbcc21e235df180478842ab7a6
autoprintsai-code <autoprintsai@gmail.com>
2025-09-05 03:12:10 -0700

    Updated InvestmentPlatform.tsx

--- src/components/HeliosDashboard.tsx
+++ src/components/HeliosDashboard.tsx
@@ -1,31 +1,5 @@
-import React, { useState } from 'react'
-import { FundingModal } from './FundingModal'
-import { HeliosTerminalEmbed } from './HeliosTerminalEmbed'
-import { useAuth } from './auth/AuthProvider'
-
-export function HeliosDashboard() {
-  const { account } = useAuth()
-  const [showFundingModal, setShowFundingModal] = useState(false)
-
-  const openFunding = () => {
-    setShowFundingModal(true)
-  }
-
-  const closeFunding = () => {
-    setShowFundingModal(false)
-  }
-
-  return (
-    <div className="min-h-screen bg-gray-900">
-      <HeliosTerminalEmbed
-        onFundAccount={openFunding}
+      />
+        onFundAccount={() => openFunding()}
         userBalance={account?.balance || 0}
-      />
-      
-      <FundingModal
-        isOpen={showFundingModal}
-        onClose={closeFunding}
-      />
-    </div>
-  )
-}
+      <HeliosTerminalEmbed 
+      {/* HELIOS Terminal Integration */}
