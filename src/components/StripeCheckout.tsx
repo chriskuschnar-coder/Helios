9650ff40972cac2f1e3cda4e95558e63610eac47
autoprintsai-code <autoprintsai@gmail.com>
2025-09-09 19:50:18 -0700

    Updated StripeCheckout.tsx

--- src/components/StripeCheckout.tsx
+++ src/components/StripeCheckout.tsx
@@ -5,7 +5,7 @@ import { Elements } from '@stripe/react-stripe-js'
 import { loadStripe } from '@stripe/stripe-js'
 import { PaymentElement, useStripe, useElements } from '@stripe/react-stripe-js'
 
-const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY || 'pk_test_51S1jDNFxYb2Rp5SOdBaVqGD29UBmOLc9Q3Amj5GBVXY74H1TS1Ygpi6lamYt1cFe2Ud4dBn4IPcVS8GkjybKVWJQ00h661Fiq6')
+const stripePromise = loadStripe('pk_live_YOUR_NEW_PUBLISHABLE_KEY_HERE')
 
 interface StripeCheckoutProps {
   productId?: string
