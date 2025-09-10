5afc7c969c068c3283fc36c1376f0ea69316ad6a
autoprintsai-code <autoprintsai@gmail.com>
2025-09-10 13:58:06 -0700

    Remove unused files from the project

--- src/components/EnvCheck.tsx
+++
@@ -1,7 +0,0 @@
-import React, { useEffect, useState } from 'react'
-import { AlertCircle, CheckCircle, Key, Globe } from 'lucide-react'
-
-export function EnvCheck() {
-  // Don't show anything - environment variables are handled in deployment
-  return null
-}
